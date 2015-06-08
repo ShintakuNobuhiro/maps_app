34085316a4ffb369f2131b4634f594c8f5c11c9b
ukyankyan11@gmail.com <ukyankyan11@gmail.com>
2015-06-08 12:42:49 +0000

    Create Root Path

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "Static pages" do
 
   describe "Home page" do
-    before { visit '/static_pages/home' }
+    before { visit root_path }
   
     it "should have the content 'HomePage'" do
       expect(page).to have_content('ようこそ')
@@ -16,7 +16,7 @@ describe "Static pages" do
   end
   
   describe "About page" do
-    before { visit '/static_pages/about' }
+    before { visit about_path }
     
     it "should have the content 'About'" do
       expect(page).to have_content('About')
@@ -25,6 +25,27 @@ describe "Static pages" do
     it "should have the right title" do
       expect(page).to have_title("About")
     end
-    
+  end
+  
+  describe "Contact page" do
+    before { visit contact_path }
+    it "should have the content 'Contact'" do
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      expect(page).to have_title("Contact")
+    end
+  end
+  
+  describe "Help page" do
+    before { visit help_path }
+    it "should have the content 'Help'" do
+      expect(page).to have_content('Help')
+    end
+
+    it "should have the title 'Help'" do
+      expect(page).to have_title("Help")
+    end
   end
 end
